2235bba556a963506e6e07379d1dfec522bae074
Florent Beaurain <beaurain.florent@protonmail.com>
2018-03-11 13:47:15 +0100

    fix album create

--- MyDisco/app/models/album.rb
+++ MyDisco/app/models/album.rb
@@ -2,19 +2,12 @@ class Album < ApplicationRecord
   attr_accessor :album_params
   attr_accessor :discogs
 
-  before_destroy do
-    self.genres.clear
-    self.artists.clear
-    self.streams.clear
-  end
-
-  before_save :find_album_on_discogs
-  before_save :set_name, unless: Proc.new { @album.nil? }
-  before_save :set_year, unless: Proc.new { @album.nil? }
-  before_save :set_cover_url, unless: Proc.new { @album.nil? }
-  before_save :set_artists, unless: Proc.new { @album.nil? }
-  before_save :set_genres, unless: Proc.new { @album.nil? }
-  before_save :set_streams, unless: Proc.new { @album.nil? }
+  before_create :set_name, unless: Proc.new { @album.nil? }
+  before_create :set_year, unless: Proc.new { @album.nil? }
+  before_create :set_cover_url, unless: Proc.new { @album.nil? }
+  before_create :set_artists, unless: Proc.new { @album.nil? }
+  before_create :set_genres, unless: Proc.new { @album.nil? }
+  before_create :set_streams, unless: Proc.new { @album.nil? }
 
   has_many :album_genres
   has_many :genres, through: :album_genres, dependent: :destroy
@@ -23,15 +16,17 @@ class Album < ApplicationRecord
   has_many :album_streams
   has_many :streams, through: :album_streams, dependent: :destroy
 
+  validate :album_is_present_on_discogs
+
   private
 
-  def find_album_on_discogs
+  def album_is_present_on_discogs
     discogs_albums = discogs.search(album_params[:info], per_page: 5, type: :release).results
 
     if discogs_albums.first.nil?
-      errors.add(:global, "can't find album on discogs")
+      self.errors.add(:base, "can't find album on discogs")
     else
-      @album = discogs.get_release(discogs_albums.first.id)
+      @album = discogs.get_release(discogs_albums.first&.id)
     end
   end
 
@@ -48,13 +43,13 @@ class Album < ApplicationRecord
   end
 
   def set_artists
-    @album.artists.each do |artist|
+    @album.artists&.each do |artist|
       self.artists.where(name: artist.name).first_or_create
     end
   end
 
   def set_genres
-    @album.styles.each do |style|
+    @album.styles&.each do |style|
       self.genres.where(name: style).first_or_create
     end
   end
